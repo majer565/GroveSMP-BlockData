3f6b7efe8f78c88d0650d7edf706e87318d5b6e3
Filip Majewski <majerihno@MacBook-Pro-Filip.local>
2023-03-06 16:18:43 +0100

    Added project structure

---
+++ src/main/java/com/filipmajewski/grovesmpblockdata/GroveSMP_BlockData.java
@@ -0,0 +1,17 @@
+package com.filipmajewski.grovesmpblockdata;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GroveSMP_BlockData extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
